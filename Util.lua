46d5284959ca302d9fa3d7b7d5bcb71532aaed7b
leonthemisfit <leonthemisfit@gmail.com>
2017-06-11 01:14:45 -0400

    Added Util.lua with slice, rest, list, and deep_copy table functions

---
+++ Util.lua
@@ -0,0 +1,33 @@
+function table.slice(tbl, x, y)
+  local ntbl = {}
+  for i = x, y or #tbl do
+    ntbl[#ntbl+1] = tbl[i]
+  end
+  return ntbl
+end
+
+function table.rest(tbl)
+  if #tbl < 2 then
+    return {}
+  else
+    return table.slice(tbl, 2)
+  end
+end
+
+function table.deep_copy(old_table, new_table)
+  new_table = new_table or {}
+  for k,v in pairs(old_table) do
+    if type(v) == "table" then
+      new_table[k] = table.deep_copy(v)
+    else
+      new_table[k] = v
+    end
+  end
+  return new_table
+end
+
+function table.list(tbl)
+  for k,v in pairs(tbl) do
+    print(tostring(k) .. ": " .. tostring(v))
+  end
+end
