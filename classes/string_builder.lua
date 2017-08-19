1b3d5462199cc45af2892fe65bfdce85661823d4
leonthemisfit <leonthemisfit@gmail.com>
2017-08-18 20:05:27 -0400

    Added 'string_builder' class

---
+++ classes/string_builder.lua
@@ -0,0 +1,45 @@
+local class = require("../class")
+
+local errors = {
+  TYPE_ERROR = "Specified value is not a string or string_builder."
+}
+
+local string_builder = class("string_builder")
+
+string_builder:add_readonly_property("string", "")
+
+string_builder:add_constructor({"string"}, function (self, s)
+  if class.type(s) == "string" then
+    self.privates.string = s
+  else
+    error(errors.TYPE_ERROR)
+  end
+end)
+
+string_builder:add_overloaded_method("append", {"string"}, function (self, s)
+  self.privates.string = self.string .. s
+  return self.string
+end)
+
+string_builder:add_overloaded_method("append", {"string_builder"}, function (self, s)
+  return self:append(s.string)
+end)
+
+string_builder:add_overloaded_method("prepend", {"string"}, function (self, s)
+  self.privates.string = s .. self.string
+  return self.string
+end)
+
+string_builder:add_overloaded_method("prepend", {"string_builder"}, function (self, s)
+  return self:prepend(s.string)
+end)
+
+string_builder:add_infix_method("<<", function (self, s)
+  return self:prepend(s)
+end)
+
+string_builder:add_infix_method(">>", function (self, s)
+  return self:append(s)
+end)
+
+return string_builder
