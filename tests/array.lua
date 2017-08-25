a5138da96275650094cb0e64b3b9757f5181182b
leonthemisfit <leonthemisfit@gmail.com>
2017-08-25 13:15:40 -0400

    Added length metamethod testing for 'array'

--- tests/array.lua
+++ tests/array.lua
@@ -8,6 +8,7 @@ assert(a[2] == "")
 assert(a[3] == "")
 assert(a.type == "string")
 assert(a.length == 3)
+assert(#a == 3)
 
 a[1] = "Hello"
 a[2] = "World"
@@ -22,6 +23,7 @@ local b = a.rest
 
 assert(b.type == "string")
 assert(b.length == 2)
+assert(#b == 2)
 assert(b.first == "World")
 assert(b[1] == "World")
 assert(b[2] == "!")
@@ -30,6 +32,7 @@ local c = a:slice(2)
 
 assert(c.type == "string")
 assert(c.length == 2)
+assert(#c == 2)
 assert(c.first == "World")
 assert(c[1] == "World")
 assert(c[2] == "!")
@@ -38,6 +41,7 @@ local d = a:slice(1, 2)
 
 assert(d.type == "string")
 assert(d.length == 2)
+assert(#d == 2)
 assert(d.first == "Hello")
 assert(d[1] == "Hello")
 assert(d[2] == "World")
@@ -46,6 +50,7 @@ local e = array {1, 2, 3}
 
 assert(e.type == "number")
 assert(e.length == 3)
+assert(#e == 3)
 assert(e.first == 1)
 assert(e[1] == 1)
 assert(e[2] == 2)
@@ -59,6 +64,7 @@ local f = e:map(function (v) return v * 2 end)
 
 assert(f.type == "number")
 assert(f.length == 3)
+assert(#f == 3)
 assert(f.first == 2)
 assert(f[1] == 2)
 assert(f[2] == 4)
@@ -77,15 +83,18 @@ local g = array {
 
 assert(g.type == "array")
 assert(g.length == 2)
+assert(#g == 2)
 
 assert(g[1].type == "number")
 assert(g[1].length == 3)
+assert(#g[1] == 3)
 assert(g[1][1] == 1)
 assert(g[1][2] == 2)
 assert(g[1][3] == 3)
 
 assert(g[2].type == "string")
 assert(g[2].length == 3)
+assert(#g[2] == 3)
 assert(g[2][1] == "a")
 assert(g[2][2] == "b")
 assert(g[2][3] == "c")
@@ -102,6 +111,7 @@ end
 
 assert(h.type == "number")
 assert(h.length == 7)
+assert(#h == 7)
 
 assert(h:any(even))
 assert(h 'any' (even))
@@ -117,16 +127,19 @@ local i = h 'where' (even)
 
 assert(i.type == "number")
 assert(i.length == 4)
+assert(#i == 4)
 
 local j = h 'where_value' (2)
 
 assert(j.type == "number")
 assert(j.length == 2)
+assert(#j == 2)
 
 local k = h.unique
 
 assert(k.type == "number")
 assert(k.length == 5)
+assert(#k == 5)
 assert(k[1] == 1)
 assert(k[2] == 2)
 assert(k[3] == 3)
